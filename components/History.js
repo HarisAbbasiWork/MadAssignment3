6534719f089bc1ae01226e0df015efb9e7095074
Haris Abbasi <harisbakhabarpk@gmail.com>
2020-12-19 14:08:53 +0500

    Update History.js

--- components/History.js
+++ components/History.js
@@ -4,48 +4,38 @@ import { DataTable } from 'react-native-paper';
 import Constants from 'expo-constants';
 
 
-
  export default function  ResultScreen ({route,navigation}) {
      
-     const [meeting2, setMeeting2]=useState(route.params.meeting)
-     const [history, setHistory]=useState([])
-     const deletehistory=()=>{
-       setHistory({
-         price: "",
-         discount:"",
-         fprice: ""
-       })
+     const [history, setHistory]=useState(route.params.meeting)
+     
+     const removehistory=()=>{
+       
        navigation.navigate('Start',{
             remove:true
           })
      }
      
-     useEffect(() => {
-       
-       setHistory(history.push(meeting2))
-       console.log("I am here",history.length,meeting2.length)
      
-     },[]);
      
   return (
     <View style={styles.container}>
     <View>
     
     <FlatList
-        data={meeting2}
+        data={history}
         renderItem={({item})=>(<View>
         
         <DataTable>
     <DataTable.Header>
-      <DataTable.Title>Price</DataTable.Title>
+      <DataTable.Title >Price</DataTable.Title>
       <DataTable.Title >Discount</DataTable.Title>
       <DataTable.Title >Final Price</DataTable.Title>
     </DataTable.Header>
 
     <DataTable.Row>
-      <DataTable.Cell> {item.price}</DataTable.Cell>
-      <DataTable.Cell >{item.discount}%</DataTable.Cell>
-      <DataTable.Cell >{item.fprice}</DataTable.Cell>
+      <DataTable.Cell style={styles.fortext2}> {item.price}</DataTable.Cell>
+      <DataTable.Cell style={styles.fortext2}>{item.discount}%</DataTable.Cell>
+      <DataTable.Cell style={styles.fortext2}>{item.fprice}</DataTable.Cell>
     </DataTable.Row>
     <DataTable.Pagination
       page={1}
@@ -61,7 +51,7 @@ import Constants from 'expo-constants';
         keyExtractor={(item, index) => item.id}
         
       />
-      <Button title="Delete History" onPress={deletehistory}/>
+      <Button title="Delete History" onPress={removehistory}/>
     </View>
      
     </View>
@@ -75,6 +65,14 @@ import Constants from 'expo-constants';
     alignItems: 'center',
     backgroundColor: '#10c99e',
   },
+  fortext: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 15,
+    backgroundColor: '#E3DC02'
+  },
 
   
 });
+
+
